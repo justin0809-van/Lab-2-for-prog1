bff234c3aed8f01427c1a93c13adf07d6a19a8d9
justin <justingeloca@gmail.com>
2025-09-04 21:45:14 -0400

    Answered questions up to 44.

--- TicketMachine.java
+++ TicketMachine.java
@@ -23,9 +23,9 @@ public class TicketMachine
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
-    public TicketMachine(int cost)
+    public TicketMachine(int price)
     {
-        price = cost;
+        price = price;
         balance = 0;
         total = 0;
     }
@@ -54,7 +54,7 @@ public class TicketMachine
     {
         balance = balance + amount;
     }
-
+    
     /**
      * Print a ticket.
      * Update the total collected and
